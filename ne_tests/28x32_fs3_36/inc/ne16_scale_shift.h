6cf84377f48a74c17dbf42b844c319cfb756ee17
Arpan Prasad <prasadar@kohtopa.ee.ethz.ch>
2021-10-11 12:43:38 +0200

    added repeated tests

--- ne_tests/28x32_fs3_36/inc/ne16_scale_shift.h
+++
@@ -1,3 +0,0 @@
-#define STIM_NQS_SIZE 1
-// scale_shift [32]
-int8_t ne16_scale_shift[] = { 0 };
---
+++ ne_tests/ki1_ko1_w1_h1_fs3_repeated/inc/ne16_infeat.h
@@ -0,0 +1,14 @@
+// infeat [3 x 3 x 1]
+#define STIM_X_SIZE 9
+uint8_t __attribute__((section(".heapsram"))) ne16_infeat[] = {
+  0x54,
+  0xc5,
+  0x64,
+  0xca,
+  0xa3,
+  0xc5,
+  0xcb,
+  0xd5,
+  0x28
+};
+
